6d0d14f2daeb361cb17c209deb0296e7e5f76fd8
devdaim6 <daimdev6@gmail.com>
2023-09-03 20:37:48 +0530

    Feed Component almost Donee

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
-const Feed = () => {
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div>Feed</div>
-  )
-}
+    <div className="mt-16 prompt_layout">
+      {Array.isArray(data)?(data.map((prompt) => (
+        <PromptCard
+          key={prompt._id}
+          prompt={prompt}
+          handleTagClick={handleTagClick}
+        />
+      ))):
+      <>
+      </>}
+    </div>
+  );
+};
+const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+  const [prompts, setPrompts] = useState("");
 
-export default Feed
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPrompts = async () => {
+      const res = await fetch("/api/prompt");
+      const data = await res.json();
+      setPrompts(data);
+      // console.log(data)
+    };
+    fetchPrompts();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full  flex-center">
+        <input
+          type="text"
+          placeholder="Search for a @username or tag"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={prompts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
