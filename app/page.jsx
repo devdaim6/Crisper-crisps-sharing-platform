91c51dfbae906c3b9113ee8c60f60fcf37d8b57e
devdaim6 <daimdev6@gmail.com>
2023-09-03 09:39:32 +0530

    deployed

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import Feed from '@components/Feed'
+const Home = () => {
+  return (
+   <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center">Discover and Share
+    <br className=" "/>
+    <span className="orange_gradient text-center">User-Powered Prompts</span></h1>
+    <p className="desc text-center">
+      Crisper is an Open-source Prompting platform for modern world to create and share their valuable prompts.
+    </p>
+
+
+    {/* feed */}
+    <Feed/>
+   </section>
+  )
+}
+
+export default Home
